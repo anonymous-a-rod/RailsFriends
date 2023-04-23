b224c12787272cb55c5906b849b04233ea484656
Aaron <32913725+anonymous-a-rod@users.noreply.github.com>
2023-04-22 23:04:53 -0400

    action#methods and site layout

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,24 @@
+class HomeController < ApplicationController
+
+  def index
+  end
+
+  def about
+  end
+
+  def careers
+  end
+
+  def contact
+  end
+
+  def integration
+  end
+
+  def faqs
+  end
+
+  def services
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'about', to: 'home#about'
+  get 'contact', to: 'home#contact'
+  get 'careers', to: 'home#careers'
+  get 'integration', to: 'home#integration'
+  get 'faqs', to: 'home#faqs'
+  get 'services', to: 'home#services'
+  get 'testimonials', to: 'home#testimonials'
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
 end
