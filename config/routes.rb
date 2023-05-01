6c76f8bdb5a0e19c4463b740fa73345e6a3b70f6
Aaron <32913725+anonymous-a-rod@users.noreply.github.com>
2023-05-01 11:20:01 -0400

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :employees
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'about', to: 'home#about'
